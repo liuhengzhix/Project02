c33969e71c225070c6521bde0ce8b2a5c2565fb9
liuhengzhix <l2731286559@163.com>
2021-02-15 19:56:30 +0800

    392 episode

---
+++ day01/src/inid/lhz/day01/demo02/Demo03FileReader.java
@@ -0,0 +1,19 @@
+package inid.lhz.day01.demo02;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+/*
+    FileReader可以读取IDE默认编码格式(UTF-8) 的文件
+    FileReader读取系统默认编码(中文GBK) 会产生乱码eee
+ */
+public class Demo03FileReader {
+    public static void main(String[] args) throws IOException {
+        FileReader fr = new FileReader("D:\\bilibili\\GBK.txt");
+        int len = 0;
+        while ((len = fr.read()) != -1) {
+            System.out.println(len);
+        }
+        fr.close();
+    }
+}
