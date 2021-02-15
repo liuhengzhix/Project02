acbfa9e3c2cc74a6bd063dc637b08e605d1f52db
liuhengzhix <l2731286559@163.com>
2021-02-15 17:57:17 +0800

    the sec commit

--- day01/src/inid/lhz/day01/demo01/HelloWorld.java
+++ day01/src/inid/lhz/day01/demo01/HelloWorld.java
@@ -3,5 +3,6 @@ package inid.lhz.day01.demo01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("确认是否同步");
     }
 }
