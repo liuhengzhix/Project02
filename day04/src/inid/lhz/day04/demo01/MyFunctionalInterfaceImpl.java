0e8b729480c3ed814563e018b5dcb36f2c569d4b
liuhengzhix <l2731286559@163.com>
2021-02-18 12:15:47 +0800

    开启函数式编程

---
+++ day04/src/inid/lhz/day04/demo01/MyFunctionalInterfaceImpl.java
@@ -0,0 +1,12 @@
+package inid.lhz.day04.demo01;
+
+/*
+    接口的实现类
+ */
+public class MyFunctionalInterfaceImpl implements MyFunctionalInterface{
+
+    @Override
+    public void method() {
+
+    }
+}
